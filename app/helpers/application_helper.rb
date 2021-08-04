54dc203414fe185ace5a18effaff11d0b6fa64d4
Daniel Rocha <daniel1990rocha@gmail.com>
2021-08-04 10:19:52 -0300

    alterações na gemfile

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
     def date_br(date_us)
         date_us.strftime("%d/%m/%Y")
     end    
+
+    def locale(locale)
+      I18n.locale == :en ? 'U.S.A' : 'Português/BR'
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,7 @@ Rails.application.routes.draw do
   resources :mining_types
   get 'welcome/index'
   # get 'inicio', to: 'welcome#index'
-
-
   resources :coins
-
-  root to: "welcome#index"
+  root to: 'welcome#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
